84fbdf127c5c2425b6dfc4222300a7b3ff78f963
Ricardo <itwasmydoppelganger@gmail.com>
2018-09-04 13:37:13 -0300

    Blah

---
+++ Applicc/Controllers/PessoaController.cs
@@ -0,0 +1,58 @@
+﻿using Applicc.Models;
+using Applicc.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Applicc.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public static List<PessoaModel> Pessoas { get; set; }
+
+        public PessoaController()
+        {
+            if(Pessoas == null)
+            {
+                Pessoas = new List<PessoaModel>
+                {
+                    new PessoaModel
+                    {
+                        Nome = "John Doe",
+                        Nascimento = new DateTime(2000, 08, 21),
+                        Sexo = Sexo.Masculino
+                    },
+                    new PessoaModel
+                    {
+                        Nome = "Jane Doe",
+                        Nascimento = new DateTime(2000, 08, 21),
+                        Sexo = Sexo.Feminino
+                    }
+                };
+            }
+        }
+
+        public ActionResult Index()
+        {
+            return View(Pessoas);
+        }
+
+        public ActionResult New()
+        {
+            return View("New", new PessoaModel());
+        }
+
+        public ActionResult Salvar(PessoaModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Pessoas.Add(model);
+
+            }
+
+            return View("Index", Pessoas);
+        }
+    }
+}
---
+++ Applicc/Models/PessoaModel.cs
@@ -0,0 +1,20 @@
+﻿using Applicc.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Applicc.Models
+{
+    public class PessoaModel
+    {
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime Nascimento { get; set; }
+        [Required]
+        public Sexo Sexo { get; set; }
+    }
+}
